e84a097bee898dc5bfc6728ca43fbf3f4cf95f75
Bibek Gadel <vieekgadel49@gmail.com>
2026-02-11 20:34:57 +0545

    wrapped by context

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CartContextProvider } from './Context/CartContext.jsx'
+import { UserContextProvider } from './Context/UserContext.jsx'
+import { ProductContextProvider } from './Context/ProductContext.jsx'
+import { WishlistProvider } from './Context/WishListContex.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ <UserContextProvider>
+ <WishlistProvider>
+   <ProductContextProvider>
+    <CartContextProvider>
+    <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </CartContextProvider>
+   </ProductContextProvider>
+   </WishlistProvider>
+ </UserContextProvider>
 )
