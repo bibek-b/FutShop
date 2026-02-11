04b139c5540b5985a427c14dd9a897d82f845564
Bibek Gadel <vieekgadel49@gmail.com>
2026-02-11 20:39:58 +0545

    minor changes

--- Server/routes/productRoutes.js
+++ Server/routes/productRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import { CreateProduct, GetProducts, GetProduct, UpdateProduct, DeleteProduct  } from '../controller/productsController.js';
-import authMiddleware from '../Middleware/authMiddleware.js';
+import { CreateProduct, GetProducts, GetProduct, UpdateProduct, DeleteProduct, GetProductByCategory, GetProductByUserId  } from '../controller/productsController.js';
+// import authMiddleware from '../Middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post("/create", authMiddleware, CreateProduct);
+router.post("/create", CreateProduct);
 router.get("/", GetProducts);
 router.get("/:productId", GetProduct);
-router.put("/:productId", UpdateProduct);
-router.delete("/:productId", DeleteProduct);
+router.get("/:userId", GetProductByUserId);
+router.get("/category/:category", GetProductByCategory);
+router.put("/update/:productId", UpdateProduct);
+router.delete("/delete/:productId", DeleteProduct);
 
 export default router;
