6e97a85137ca22175b05e2da264c1af5da98fdc6
Bibek Gadel <vieekgadel49@gmail.com>
2026-02-11 20:32:39 +0545

    dummy data added

---
+++ Client/src/dummyData.js
@@ -0,0 +1,35 @@
+export const dummyData = [
+    {
+        id: 1,
+        title: "Messi Boot",
+        productCategory: "Football Boots",
+        price: 2999,
+        rating: 4.9,
+        image: "./assets/lamineSpain.jpg"
+    },
+    {
+        id: 2,
+        title: "GK Glove",
+        productCategory: "Football Gloves",
+        price: 1899,
+        rating: 4.4,
+        image: "./assets/Gloves.webp"
+    },
+     {
+        id: 3,
+        title: "Barca Jersey",
+        productCategory: "Football Jerseys",
+        price: 2499,
+        rating: 5,
+        image: "./assets/jerseys.jpg"
+    },
+    {
+        id: 4,
+        title: "UCL Ball",
+        productCategory: "Football Balls",
+        price: 1599,
+        rating: 5,
+        image: "./assets/heroImgs/heroball.jpg"
+    },
+
+]
